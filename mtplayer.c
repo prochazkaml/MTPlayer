3a4a97b25b48da59b8da8445c4ec34aad28f81b1
Michal Procházka <prochazka2003@gmail.com>
2021-08-21 07:15:44 +0200

    Moved all tracker vars into a single struct

--- mtplayer.c
+++ mtplayer.c
@@ -8,39 +8,24 @@
 #define vibtablesize 32
 #define vibtabledepth (IBN * 8)
 
-uint16_t *data, *rowdata;
+songstatus_t s;
 
-uint8_t patterns, channels, ordertable[256];
-int row = 0x3F, order = -1, orders, tempo, tempotick = 1, audiospeed, audiotick = 1;
-
-typedef struct {
-	int16_t interval, freq, portafreq;
-	uint8_t enabled, active;
-
-	uint8_t note, effect, parm1, parm2;
-	uint8_t lastnote, effectraw, portadelta;
-	uint8_t vibspeed, vibdepth, vibindex;
-
-	int ctr;
-} channel_t;
-
-channel_t channel[12];
 short notesHz[maxnote * IBN];
 short VibTable[vibtablesize];
 
 int MTPlayer_Init(uint8_t *filedata) {
 	int i, ch;
 	
-	patterns = filedata[0x5C];
+	s.patterns = filedata[0x5C];
 
-	if((channels = filedata[0x5D]) > maxchannels) {
+	if((s.channels = filedata[0x5D]) > maxchannels) {
 		return 0;
 	}
 
-	for(orders = 0; orders < 256; orders++) {
-		if(filedata[0x5F + orders] == 0xFF) break;
+	for(s.orders = 0; s.orders < 256; s.orders++) {
+		if(filedata[0x5F + s.orders] == 0xFF) break;
 
-		ordertable[orders] = filedata[0x5F + orders];
+		s.ordertable[s.orders] = filedata[0x5F + s.orders];
 	}
 
 	double interval = pow(2, 1.0 / (12 * IBN));
@@ -61,18 +46,23 @@ int MTPlayer_Init(uint8_t *filedata) {
 		notesHz[i] = round(temphz);
 	}
 
-	tempo = (channels > 4) ? channels : 4;
+	s.tempo = (s.channels > 4) ? s.channels : 4;
 
 	for(ch = 0; ch < maxchannels; ch++) {
-		channel[ch].enabled = 1;
-		channel[ch].active = 0;
+		s.channel[ch].enabled = 1;
+		s.channel[ch].active = 0;
 	}
 
 	for(i = 0; i < vibtablesize; i++) {
 		VibTable[i] = round(vibtabledepth * sin(((double)i) * M_PI / vibtablesize * 2));
 	}
 
-	data = (uint16_t *)(filedata + 0x15F);
+	s.data = (uint16_t *)(filedata + 0x15F);
+
+	s.row = 0x3F;
+	s.order = -1;
+	s.tempotick = 1;
+	s.audiotick = 1;
 
 	return 1;
 }
@@ -80,141 +70,141 @@ int MTPlayer_Init(uint8_t *filedata) {
 void _MTPlayer_ProcessTick() {
 	int i, ch;
 
-	if(!--tempotick) {
+	if(!--s.tempotick) {
 		// Process a new row
 
-		rowdata += channels;
+		s.rowdata += s.channels;
 
-		if(++row >= 0x40) {
+		if(++s.row >= 0x40) {
 			// Process the next order
 
-			if(++order >= orders) order = 0;
+			if(++s.order >= s.orders) s.order = 0;
 
-			rowdata = data + ordertable[order] * 64 * channels;
+			s.rowdata = s.data + s.ordertable[s.order] * 64 * s.channels;
 
-			row = 0;
+			s.row = 0;
 		}
 
-		for(ch = 0; ch < channels; ch++) {
-			channel[ch].note = rowdata[ch] >> 9;
-			channel[ch].effectraw = rowdata[ch] & 0x1FF;
-			channel[ch].effect = (rowdata[ch] >> 6) & 7;
+		for(ch = 0; ch < s.channels; ch++) {
+			s.channel[ch].note = s.rowdata[ch] >> 9;
+			s.channel[ch].effectraw = s.rowdata[ch] & 0x1FF;
+			s.channel[ch].effect = (s.rowdata[ch] >> 6) & 7;
 
-			if(channel[ch].effect == 0 || channel[ch].effect == 4) {
-				channel[ch].parm1 = (rowdata[ch] >> 3) & 7;
-				channel[ch].parm2 = rowdata[ch] & 7;
+			if(s.channel[ch].effect == 0 || s.channel[ch].effect == 4) {
+				s.channel[ch].parm1 = (s.rowdata[ch] >> 3) & 7;
+				s.channel[ch].parm2 = s.rowdata[ch] & 7;
 			} else {
-				channel[ch].parm1 = rowdata[ch] & 63;
-				channel[ch].parm2 = 0;
+				s.channel[ch].parm1 = s.rowdata[ch] & 63;
+				s.channel[ch].parm2 = 0;
 			}
 
-			if(channel[ch].note == 127) {
-				channel[ch].active = 0;
-			} else if(channel[ch].note && channel[ch].note <= maxnote && channel[ch].effect != 3) {
-				channel[ch].active = 1;
-				channel[ch].interval = channel[ch].note * IBN;
-				channel[ch].freq = notesHz[channel[ch].interval];
-				channel[ch].lastnote = channel[ch].note;
-				channel[ch].vibindex = 0;
-				channel[ch].ctr = 0;
+			if(s.channel[ch].note == 127) {
+				s.channel[ch].active = 0;
+			} else if(s.channel[ch].note && s.channel[ch].note <= maxnote && s.channel[ch].effect != 3) {
+				s.channel[ch].active = 1;
+				s.channel[ch].interval = s.channel[ch].note * IBN;
+				s.channel[ch].freq = notesHz[s.channel[ch].interval];
+				s.channel[ch].lastnote = s.channel[ch].note;
+				s.channel[ch].vibindex = 0;
+				s.channel[ch].ctr = 0;
 			}
 
-			if(channel[ch].effect) switch(channel[ch].effect) {
+			if(s.channel[ch].effect) switch(s.channel[ch].effect) {
 				case 3:
-					if(channel[ch].note && channel[ch].note <= maxnote)
-						channel[ch].portafreq = notesHz[channel[ch].note * IBN];
+					if(s.channel[ch].note && s.channel[ch].note <= maxnote)
+						s.channel[ch].portafreq = notesHz[s.channel[ch].note * IBN];
 					
-					if(channel[ch].parm1) channel[ch].portadelta = channel[ch].parm1;
+					if(s.channel[ch].parm1) s.channel[ch].portadelta = s.channel[ch].parm1;
 					break;
 
 				case 4:
-					if(channel[ch].parm1) channel[ch].vibspeed = channel[ch].parm1;
-					if(channel[ch].parm2) channel[ch].vibdepth = channel[ch].parm2;
-					channel[ch].vibindex += channel[ch].vibspeed;
-					channel[ch].vibindex %= vibtablesize;
+					if(s.channel[ch].parm1) s.channel[ch].vibspeed = s.channel[ch].parm1;
+					if(s.channel[ch].parm2) s.channel[ch].vibdepth = s.channel[ch].parm2;
+					s.channel[ch].vibindex += s.channel[ch].vibspeed;
+					s.channel[ch].vibindex %= vibtablesize;
 					break;
 
 				case 5:
-					order = channel[ch].parm1 - 1;
-					row = 0x3F;
+					s.order = s.channel[ch].parm1 - 1;
+					s.row = 0x3F;
 					break;
 
 				case 6:
-					if(channel[ch].parm1 && channel[ch].parm1 < 0x40) {
-						row = channel[ch].parm1 - 1;
-						order++;
+					if(s.channel[ch].parm1 && s.channel[ch].parm1 < 0x40) {
+						s.row = s.channel[ch].parm1 - 1;
+						s.order++;
 
-						if(++order >= orders) order = 0;
+						if(++s.order >= s.orders) s.order = 0;
 
-						rowdata = data + (ordertable[order] * 64 + row) * channels;
+						s.rowdata = s.data + (s.ordertable[s.order] * 64 + s.row) * s.channels;
 					} else {
-						row = 0x3F;
+						s.row = 0x3F;
 					}
 					break;
 
 				case 7:
-					tempo = channel[ch].parm1;
+					s.tempo = s.channel[ch].parm1;
 					break;
 			}
 		}
 
-		tempotick = tempo;
+		s.tempotick = s.tempo;
 	} else {
 		// Process effects
 
-		for(ch = 0; ch < channels; ch++) {
-			if(channel[ch].effectraw) {
-				switch(channel[ch].effect) {
+		for(ch = 0; ch < s.channels; ch++) {
+			if(s.channel[ch].effectraw) {
+				switch(s.channel[ch].effect) {
 					case 0:
-						switch((tempo - tempotick) % 3) {
+						switch((s.tempo - s.tempotick) % 3) {
 							case 0:
-								channel[ch].interval = channel[ch].lastnote * IBN;
+								s.channel[ch].interval = s.channel[ch].lastnote * IBN;
 								break;
 
 							case 1:
-								channel[ch].interval = (channel[ch].lastnote + channel[ch].parm1) * IBN;
+								s.channel[ch].interval = (s.channel[ch].lastnote + s.channel[ch].parm1) * IBN;
 								break;
 
 							case 2:
-								channel[ch].interval = (channel[ch].lastnote + channel[ch].parm2) * IBN;
+								s.channel[ch].interval = (s.channel[ch].lastnote + s.channel[ch].parm2) * IBN;
 								break;
 						}
 
-						channel[ch].freq = notesHz[channel[ch].interval];
+						s.channel[ch].freq = notesHz[s.channel[ch].interval];
 						break;
 
 					case 1:
-						channel[ch].freq += channel[ch].parm1;
+						s.channel[ch].freq += s.channel[ch].parm1;
 						// I mean, it's not _really_ necessary to clamp the upper frequency limit
 						break;
 
 					case 2:
-						channel[ch].freq -= channel[ch].parm1;
-						if(channel[ch].freq < 20) channel[ch].freq = 20;
+						s.channel[ch].freq -= s.channel[ch].parm1;
+						if(s.channel[ch].freq < 20) s.channel[ch].freq = 20;
 						break;
 
 					case 3:
-						if(channel[ch].freq < channel[ch].portafreq) {
-							channel[ch].freq += channel[ch].portadelta;
+						if(s.channel[ch].freq < s.channel[ch].portafreq) {
+							s.channel[ch].freq += s.channel[ch].portadelta;
 
-							if(channel[ch].freq > channel[ch].portafreq) {
-								channel[ch].freq = channel[ch].portafreq;
+							if(s.channel[ch].freq > s.channel[ch].portafreq) {
+								s.channel[ch].freq = s.channel[ch].portafreq;
 							}
 						}
 						
-						if(channel[ch].freq > channel[ch].portafreq) {
-							channel[ch].freq -= channel[ch].portadelta;
+						if(s.channel[ch].freq > s.channel[ch].portafreq) {
+							s.channel[ch].freq -= s.channel[ch].portadelta;
 
-							if(channel[ch].freq < channel[ch].portafreq) {
-								channel[ch].freq = channel[ch].portafreq;
+							if(s.channel[ch].freq < s.channel[ch].portafreq) {
+								s.channel[ch].freq = s.channel[ch].portafreq;
 							}
 						}
 						break;
 
 					case 4:
-						channel[ch].freq = notesHz[channel[ch].interval + (((VibTable[channel[ch].vibindex]) * channel[ch].vibdepth) / vibtabledepth)];
-						channel[ch].vibindex += channel[ch].vibspeed;
-						channel[ch].vibindex %= vibtablesize;
+						s.channel[ch].freq = notesHz[s.channel[ch].interval + (((VibTable[s.channel[ch].vibindex]) * s.channel[ch].vibdepth) / vibtabledepth)];
+						s.channel[ch].vibindex += s.channel[ch].vibspeed;
+						s.channel[ch].vibindex %= vibtablesize;
 						break;
 				}
 			}
@@ -225,27 +215,27 @@ void _MTPlayer_ProcessTick() {
 void MTPlayer_PlayInt16(int16_t *buf, int bufsize, int audiofreq) {
 	int i, ch;
 
-	audiospeed = audiofreq / 60;
+	s.audiospeed = audiofreq / 60;
 
 	memset(buf, 0, bufsize * sizeof(short));
 
 	for(i = 0; i < bufsize; i++) {
 		// Process tick if necessary
 
-		if(!--audiotick) {
-			audiotick = audiospeed;
+		if(!--s.audiotick) {
+			s.audiotick = s.audiospeed;
 
 			_MTPlayer_ProcessTick();
 		}
 
 		// Render the sound
 
-		for(ch = 0; ch < channels; ch++) {
-			if(channel[ch].active && channel[ch].enabled) {
-				channel[ch].ctr += channel[ch].freq;
+		for(ch = 0; ch < s.channels; ch++) {
+			if(s.channel[ch].active && s.channel[ch].enabled) {
+				s.channel[ch].ctr += s.channel[ch].freq;
 
-				if((channel[ch].ctr / (audiofreq / 2)) & 1) {
-					buf[i] += 32767 / channels;
+				if((s.channel[ch].ctr / (audiofreq / 2)) & 1) {
+					buf[i] += 32767 / s.channels;
 				}
 			}
 		}
@@ -255,27 +245,27 @@ void MTPlayer_PlayInt16(int16_t *buf, int bufsize, int audiofreq) {
 void MTPlayer_PlayFloat(float *buf, int bufsize, int audiofreq) {
 	int i, ch;
 
-	audiospeed = audiofreq / 60;
+	s.audiospeed = audiofreq / 60;
 
 	memset(buf, 0, bufsize * sizeof(float));
 
 	for(i = 0; i < bufsize; i++) {
 		// Process tick if necessary
 
-		if(!--audiotick) {
-			audiotick = audiospeed;
+		if(!--s.audiotick) {
+			s.audiotick = s.audiospeed;
 
 			_MTPlayer_ProcessTick();
 		}
 
 		// Render the sound
 
-		for(ch = 0; ch < channels; ch++) {
-			if(channel[ch].active && channel[ch].enabled) {
-				channel[ch].ctr += channel[ch].freq;
+		for(ch = 0; ch < s.channels; ch++) {
+			if(s.channel[ch].active && s.channel[ch].enabled) {
+				s.channel[ch].ctr += s.channel[ch].freq;
 
-				if((channel[ch].ctr / (audiofreq / 2)) & 1) {
-					buf[i] += 1.0 / ((float)channels);
+				if((s.channel[ch].ctr / (audiofreq / 2)) & 1) {
+					buf[i] += 1.0 / ((float)s.channels);
 				}
 			}
 		}
--- mtplayer.h
+++ mtplayer.h
@@ -1,5 +1,25 @@
 #include <stdint.h>
 
+typedef struct {
+	int16_t interval, freq, portafreq;
+	uint8_t enabled, active;
+
+	uint8_t note, effect, parm1, parm2;
+	uint8_t lastnote, effectraw, portadelta;
+	uint8_t vibspeed, vibdepth, vibindex;
+
+	int ctr;
+} channel_t;
+
+typedef struct {
+	uint16_t *data, *rowdata;
+
+	uint8_t patterns, channels, ordertable[256];
+	int row, order, orders, tempo, tempotick, audiospeed, audiotick;
+
+	channel_t channel[12];
+} songstatus_t;
+
 int MTPlayer_Init(uint8_t *filedata);
 void MTPlayer_PlayInt16(int16_t *buf, int bufsize, int audiofreq);
 void MTPlayer_PlayFloat(float *buf, int bufsize, int audiofreq);
