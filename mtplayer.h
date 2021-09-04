1e959c8590d58f2e6f2441ea9b9945a51aa212ea
Michal Procházka <prochazka2003@gmail.com>
2021-09-04 10:02:20 +0200

    Fixed type for effectraw

--- mtplayer.h
+++ mtplayer.h
@@ -1,11 +1,11 @@
 #include <stdint.h>
 
 typedef struct {
-	int16_t interval, freq, portafreq;
+	int16_t interval, freq, portafreq, effectraw;
 	uint8_t enabled, active;
 
 	uint8_t note, effect, parm1, parm2;
-	uint8_t lastnote, effectraw, portadelta;
+	uint8_t lastnote, portadelta;
 	uint8_t vibspeed, vibdepth, vibindex;
 
 	int ctr;
